c005eaeac6a929a0cc457856290902ec8758372e
Maks4816 <34421603+Maks4816@users.noreply.github.com>
2019-02-18 21:54:02 +0200

    Revert "Оптимизировал твоё творение"

--- calc.py
+++ calc.py
@@ -4,17 +4,18 @@
 import sys
 from sys import exit
 
-value = None
-while value != 0:
+start = 1
+while start:
+    start = 0
 
     value = int(input(" \n 0) Выход/Exit/Вихід \n 1) Русский \n 2) English \n 3) Українська \n \n" ))
     if (value == 0):
         sys.exit()
-    elif (value == 1):
+    if (value == 1):
         import rus
-    elif (value == 2):
+    if (value == 2):
         import eng
-    elif (value == 3):
+    if (value == 3):
         import ukr
     else:
-        print("I don't know what you write, try again")
+        start = 1
